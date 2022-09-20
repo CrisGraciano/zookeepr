7d0d6e7d3d4d7c3a54243b4a000b0c56961c2312
CrisGraciano <cristobalgraciano98@gmail.com>
2022-09-19 21:14:44 -0500

    added functionallity to accept user input to json files

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+ // parse incoming string or array data
+    app.use(express.urlencoded({ extended: true}));
+    // parse incoming JSON data
+    app.use(express.json());
+
 const  { animals } = require('./data/animals');
 
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
+}
+
+function createNewAnimal(body, animalsArray) {
+    console.log(body);
+    // our function's main code will go here!
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        pat.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    // return finished code to post route for response
+    return body;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -24,6 +60,20 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
 });
+
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
+   
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray =[];
